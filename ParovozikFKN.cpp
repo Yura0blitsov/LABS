fdae2580609313a17807af06cd587e0ffab73fcf
Yura0blitsov <yura.oblitsov@gmail.com>
2021-04-30 19:24:49 +0600

    Paovozik v consoli

---
+++ ParovozikFKN.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void parovozikFKN(int i)
+{
+    for (int n = 0; n < i; n++)
+    {
+        cout << " ";
+    }
+    cout << "                                          ';';;''',''.\".\";''',,,," << endl;
+    for (int n = 0; n < i; n++)
+    {
+        cout << " ";
+    }
+    cout << "                                                                _    " << endl;
+    for (int n = 0; n < i; n++)
+    {
+        cout << " ";
+    }
+    cout << "________   ________   ________    ________   ________   |***|  | |   " << endl;
+    for (int n = 0; n < i; n++)
+    {
+        cout << " ";
+    }
+    cout << "********   ********   ********    ********   ********   |___|  \\ / " << endl;
+    for (int n = 0; n < i; n++)
+    {
+        cout << " ";
+    }
+    cout << "*5 курс*   *4 курс*   *3 курс*    *2 курс*   *1 курс*   ** Ф К Н **\\" << endl;
+    for (int n = 0; n < i; n++)
+    {
+        cout << " ";
+    }
+    cout << "******** * ******** * ********  * ******** * ******** * ***********/" << endl;
+    for (int n = 0; n < i; n++)
+    {
+        cout << " ";
+    }
+    cout << "00    00   00    00   00    00    00    00   00    00   (_)     (_) " << endl;
+    for (int n = 0; n < i; n++)
+    {
+        cout << "=";
+    }
+    cout << "====================================================================" << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int i = 0;
+    while (true)
+    {
+        parovozikFKN(i);
+        
+        system("cls");
+        i++;
+
+        if (i == 50)
+        {
+            i = 0;
+        }
+
+    }
+}
